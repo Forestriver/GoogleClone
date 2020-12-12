3ede2e455c7f72cc85481fecf1f48f08e826773e
Forestriver <vitaly.romas1@gmail.com>
2020-12-12 21:41:54 +0200

    scraper file

---
+++ Search_app/scraper.py
@@ -0,0 +1,15 @@
+import requests_html
+from requests_html import HTMLSession, HTML
+import re
+
+
+class Scraping:
+    session = HTMLSession()
+    url = 'https://github.com/Forestriver?tab=repositories'
+
+    response = session.get(url)
+
+    PageContent = response.html.html
+
+    links = response.html.absolute_links
+    print(links)
